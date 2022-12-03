d6b165bec2e2ff33ac9b74228a1a109ce866891b
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-03 09:46:44 -0300

    Add new implement to RepositorioDeAluno

---
+++ src/main/java/br/com/alura/infra/aluno/RepositorioDeAlunosEmMemoria.java
@@ -0,0 +1,32 @@
+package br.com.alura.infra.aluno;
+
+import br.com.alura.dominio.aluno.Aluno;
+import br.com.alura.dominio.aluno.CPF;
+import br.com.alura.dominio.aluno.RepositorioDeAluno;
+import br.com.alura.dominio.aluno.exceptions.AlunoNaoEncontrado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioDeAlunosEmMemoria implements RepositorioDeAluno {
+
+    List<Aluno> alunosMatriculados = new ArrayList<>();
+
+    @Override
+    public void matricular(Aluno aluno) {
+        this.alunosMatriculados.add(aluno);
+    }
+
+    @Override
+    public Aluno buscarPorCpf(CPF cpf) {
+        return alunosMatriculados.stream()
+                .filter(a -> a.getCpf().equals(cpf.getCpf()))
+                .findFirst()
+                .orElseThrow(() -> new AlunoNaoEncontrado(cpf));
+    }
+
+    @Override
+    public List<Aluno> listarTodosOsAlunosMatriculados() {
+        return this.alunosMatriculados;
+    }
+}

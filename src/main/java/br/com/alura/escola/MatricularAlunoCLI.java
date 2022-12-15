84bf14d8c9debb40cc88ca267f198c03a32e4a44
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-12-15 00:04:20 -0300

    Move MatricularAlunoCLI to external context

--- src/main/java/br/com/alura/escola/MatricularAlunoCLI.java
+++ src/main/java/br/com/alura/escola/MatricularAlunoCLI.java
@@ -1,7 +1,9 @@
-package br.com.alura.escola.academico;
+package br.com.alura.escola;
 
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
+import br.com.alura.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSeloEmMemoria;
 import br.com.alura.escola.shared.dominio.evento.PublicadorDeEventos;
 import br.com.alura.escola.academico.dominio.aluno.LogAlunoMatriculado;
 import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
@@ -15,6 +17,7 @@ public class MatricularAlunoCLI {
 
         PublicadorDeEventos publicador = new PublicadorDeEventos();
         publicador.adicionar(new LogAlunoMatriculado());
+        publicador.adicionar(new GeraSeloAlunoNovato(new RepositorioDeSeloEmMemoria()));
         MatricularAluno matricularAluno = new MatricularAluno(new RepositorioDeAlunosEmMemoria(), publicador);
         matricularAluno.executa(
                 new MatricularAlunoDto(nome, cpf, email)
